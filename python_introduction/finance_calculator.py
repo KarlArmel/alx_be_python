8b617698c816cfac4b2b08d38fd69a741517dc2a
Ngamfe Armel <armelngamfe@gmail.com>
2024-09-15 19:02:31 +0000

    named variables using snake case

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,9 +1,9 @@
-userMonthlyIncome = input("Enter your monthly income:")
-userTotalMonthlyExpenses = input("Enter your total monthly expenses:")
+monthly_income = input("Enter your monthly income:")
+monthly_expenses = input("Enter your total monthly expenses:")
 
-monthlySavings = int(userMonthlyIncome) - int(userTotalMonthlyExpenses)
-projectedSavings = monthlySavings * 12 + (monthlySavings * 12 * 0.05)
+monthly_savings = int(monthly_income) - int(monthly_expenses)
+projected_savings = monthly_savings * 12 + (monthly_savings * 12 * 0.05)
 
-print(f'Your monthly savings are ${monthlySavings}',)
-print(f'Projected savings after one year, with interest,is: ${projectedSavings:.2f}',)
+print(f'Your monthly savings are ${monthly_savings}',)
+print(f'Projected savings after one year, with interest,is: ${projected_savings:.2f}',)
 
