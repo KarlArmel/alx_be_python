51705845817ef409aae9bc730f69f1a1718ac637
Ngamfe Armel <armelngamfe@gmail.com>
2024-09-15 18:58:59 +0000

    worked on wrong calculations for the finance calculator

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -4,5 +4,6 @@ userTotalMonthlyExpenses = input("Enter your total monthly expenses:")
 monthlySavings = int(userMonthlyIncome) - int(userTotalMonthlyExpenses)
 projectedSavings = monthlySavings * 12 + (monthlySavings * 12 * 0.05)
 
-print('Your monthly savings are $',monthlySavings)
-print('Projected savings after one year, with interest,is: $',projectedSavings)
+print(f'Your monthly savings are ${monthlySavings}',)
+print(f'Projected savings after one year, with interest,is: ${projectedSavings:.2f}',)
+
