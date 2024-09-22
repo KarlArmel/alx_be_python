d8f282df1b42d3e17e67708a45539adb02b33cce
Ngamfe Armel <armelngamfe@gmail.com>
2024-09-22 19:59:44 +0000

    fix printing of results format

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -2,4 +2,12 @@ number = input("Enter a number to see its multiplication table:")
 
 for n in range(1, 11):
   result = int(number) * n
-  print(number,"*",n,"=",result)
+  print(f"{number} * {n} = {result}")
+
+# print(number,"*",n,"=",result)
+
+
+
+# name = input("enter your name")
+# print(f"Your name is {name}")
+# print("Your name is",name)
