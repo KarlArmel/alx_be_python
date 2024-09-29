cdb35794be42d722d0c51e6ab804dd168d2a4f82
Ngamfe Armel <armelngamfe@gmail.com>
2024-09-29 22:19:06 +0000

    completed task 1

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -10,4 +10,4 @@ def perform_operation(num1, num2, operation):
             return 'Cannot divide by zero'
         return num1 / num2
     else:
-        return 'Invalid operation'
+        return 'Invalid operation'  
