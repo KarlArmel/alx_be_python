d41ec7461f4d9d56c1ab408e116f1093143d78c9
KarlArmel <armelngamfe@gmail.com>
2024-10-13 10:31:05 +0000

    Update library_system.py

--- oop/library_system.py
+++ oop/library_system.py
@@ -1,67 +1,40 @@
 class Book:
-    def __init__(self, title: str, author: str):
-        """Initializes a Book instance with title and author."""
+    def __init__(self, title, author):
         self.title = title
         self.author = author
 
     def __str__(self):
-        """Returns a string representation of the book."""
-        return f"'{self.title}' by {self.author}"
+        return f"Book: {self.title} by {self.author}"
 
 
 class EBook(Book):
-    def __init__(self, title: str, author: str, file_size: int):
-        """Initializes an EBook instance with title, author, and file size."""
+    def __init__(self, title, author, file_size):
         super().__init__(title, author)
         self.file_size = file_size
 
     def __str__(self):
-        """Returns a string representation of the EBook."""
-        return f"{super().__str__()}, File Size: {self.file_size}MB"
+        return f"EBook: {self.title} by {self.author}, File Size: {self.file_size}KB"
 
 
 class PrintBook(Book):
-    def __init__(self, title: str, author: str, page_count: int):
-        """Initializes a PrintBook instance with title, author, and page count."""
+    def __init__(self, title, author, page_count):
         super().__init__(title, author)
         self.page_count = page_count
 
     def __str__(self):
-        """Returns a string representation of the PrintBook."""
-        return f"{super().__str__()}, Page Count: {self.page_count}"
+        return f"PrintBook: {self.title} by {self.author}, Page Count: {self.page_count}"
 
 
 class Library:
     def __init__(self):
-        """Initializes a Library instance with an empty list of books."""
         self.books = []
 
-    def add_book(self, book: Book):
-        """Adds a Book, EBook, or PrintBook instance to the library."""
+    def add_book(self, book):
         self.books.append(book)
 
     def list_books(self):
-        """Prints details of each book in the library."""
         for book in self.books:
             print(book)
-from library_system import Book, EBook, PrintBook, Library
 
-def main():
-    
-    my_library = Library()
-
-    classic_book = Book("Pride and Prejudice", "Jane Austen")
-    digital_novel = EBook("Snow Crash", "Neal Stephenson", 500)
-    paper_novel = PrintBook("The Catcher in the Rye", "J.D. Salinger", 234)
-
-
-    my_library.add_book(classic_book)
-    my_library.add_book(digital_novel)
-    my_library.add_book(paper_novel)
-
-    my_library.list_books()
-
-if __name__ == "__main__":
-    main()
 
 
